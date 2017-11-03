a78596ff87577584be8592c00fb987cba6b460e8
Rodrigo Muniz <muniz95@bsd.com.br>
2017-11-03 16:40:21 +0000

    Bootstrap added

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import './Header.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <Navbar collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a>Duolingo</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1}>Home</NavItem>
+                        <NavItem eventKey={2}>Words</NavItem>
+                        <NavItem eventKey={3}>Discussion</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
