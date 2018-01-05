4c4fd96a63676621d495f27464ee60c2c5e3ae4e
Rodrigo Muniz <muniz95@bsd.com.br>
2018-01-05 15:08:22 +0000

    Progressbar added

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import '../styles/ProgressBar.css'
 
-export default function ProgressBar ({progress}) {
+export default function ProgressBar ({progress = 0}) {
   return (
     <div>
-      {progress}%
+      <div class="progress">
+        <div class="progress-bar bg-duopettaja" role="progressbar" style={{width: `${progress}%`}}
+          aria-valuenow={progress} aria-valuemin="0" aria-valuemax="100">
+        </div>
+        <span>{progress}%</span>
+      </div>
     </div>
   )
 }
