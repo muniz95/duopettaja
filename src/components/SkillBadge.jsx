954c515a26dace3f67a35a75c8eda4e6c1ce1ecc
Rodrigo Muniz <muniz95@bsd.com.br>
2018-01-12 15:34:12 +0000

    Skills centered similar to Duolingo

--- src/components/SkillBadge.jsx
+++ src/components/SkillBadge.jsx
@@ -4,16 +4,15 @@ import '../styles/SkillBadge.css';
 
 export default function SkillBadge({ name, id, active }) {
   return (
-    <div>
-      <div
-        className="col-lg-3 col-md-3 col-sm-4 col-xs-4">
+    <div className="text-center">
+      {/*<div className="col-lg-3 col-md-3 col-sm-4 col-xs-4">*/}
         <LinkContainer to={'/skill'}>
           <div className={`skill center-block skill-${active ? 'active' : 'inactive'}`}>
             &nbsp;
           </div>
         </LinkContainer>
         {name}
-      </div>
+      {/*</div>*/}
     </div>
   );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import SkillBadge from '../components/SkillBadge'
+import '../styles/Home.css'
 
 export default class Home extends Component {
   constructor (props) {
@@ -12,16 +13,26 @@ export default class Home extends Component {
   componentDidMount () {
     this.setState({
       skills: [
-        { id: 1, name: 'Basics 1', active: true },
-        { id: 2, name: 'Dates', active: true },
-        { id: 3, name: 'Work', active: false },
-        { id: 4, name: 'V. Infin', active: false },
-        { id: 5, name: 'Adessive case', active: false },
-        { id: 6, name: 'Ablative case', active: false },
-        { id: 7, name: 'Allative case', active: false },
-        { id: 8, name: 'Adverbs', active: false },
-        { id: 9, name: 'Places', active: false },
-        { id: 10, name: 'Abstract', active: false },
+        [
+          { id: 1, name: 'Basics 1', active: true },
+          { id: 2, name: 'Dates', active: true },
+          { id: 3, name: 'Work', active: false }
+        ],
+        [
+          { id: 4, name: 'V. Infin', active: false }
+        ],
+        [
+          { id: 5, name: 'Adessive case', active: false },
+          { id: 6, name: 'Ablative case', active: false }
+        ],
+        [
+          { id: 7, name: 'Allative case', active: false },
+          { id: 8, name: 'Adverbs', active: false },
+          { id: 9, name: 'Places', active: false }
+        ],
+        [
+          { id: 10, name: 'Abstract', active: false }
+        ]
       ]
     })
   }
@@ -32,8 +43,15 @@ export default class Home extends Component {
         <p className="text-left">
                     Home
         </p>
-        { this.state.skills.map(skill =>
-          <SkillBadge key={skill.id} name={skill.name} id={skill.id} active={skill.active} />
+        { this.state.skills.map((row, id) =>
+          <div className="row centered" key={id}>
+          { row.map(skill =>
+            <div className={`col-md-${12 / row.length}`}>
+              <SkillBadge key={skill.id} name={skill.name} id={skill.id} active={skill.active} />
+            </div>
+          ) }
+          </div>
+          // <SkillBadge key={skill.id} name={skill.name} id={skill.id} active={skill.active} />
         ) }
       </div>
     )
