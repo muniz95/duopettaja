af8e2df5e4d0205b86bbbb87a4ee5c57f7995743
Rodrigo Muniz <rtmuniz@positivo.com.br>
2019-07-26 13:00:51 -0300

    Made header fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,14 @@ class App extends Component {
   render (): JSX.Element {
     return (
       <Router>
-        <div className="App">
-          <Header />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/skill/:id" component={Skill} />
-              <Route path="/lesson/finished" component={FinishedLesson} />
-              <Route path="/lesson/:id" component={Lesson} />
-            </Switch>
-          </div>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/skill/:id" component={Skill} />
+            <Route path="/lesson/finished" component={FinishedLesson} />
+            <Route path="/lesson/:id" component={Lesson} />
+          </Switch>
         </div>
       </Router>
     );
