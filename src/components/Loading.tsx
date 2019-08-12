9364f70afc58107da1fbc0ebd95ade665d5ae6bb
Rodrigo Muniz <rtmuniz@positivo.com.br>
2019-08-12 16:27:43 -0300

    Minor fix

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,4 +2,6 @@
 import React from "react";
 /* eslint-enable no-unused-vars */
 
-export default function Loading () { return <i className="fa fa-spinner fa-spin" style={{fontSize: "100px", color: "#20a8e9"}}></i>;}
+export default function Loading() {
+  return <i className="fa fa-spinner fa-spin" style={{fontSize: "100px", color: "#20a8e9"}}></i>;
+}
